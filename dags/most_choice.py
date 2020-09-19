7116ef0c0d28430ea7e46aef1e1d8bf55faf6889
hhk919 <53887313+hhk919@users.noreply.github.com>
2020-09-19 12:11:42 +0900

    Update most_choice.py

--- dags/most_choice.py
+++ dags/most_choice.py
@@ -4,58 +4,63 @@ from airflow.contrib.hooks.mongo_hook import MongoHook
 import logging
 from datetime import datetime
 
-def get_mongo_connection():
-    hook = MongoHook(conn_id='playrecipe_mongo')
-    return hook.get_collection(mongo_collection='playlists',mongo_db='playrecipe')
 
-def extract() :
+def get_mongodb_connection():
+    conn = MongoHook(conn_id='playrecipe_mongo').get_conn()
+    return conn
+
+
+def extract(collection_name):
+    collection = get_mongodb_connection().get_database('playrecipe').get_collection(collection_name)
     logging.info("Extract started")
     pipeline = [{"$unwind": "$playlists"}, {"$unwind": "$playlists.tracks"},
                 {"$group": {"_id": {"id": "$playlists.tracks.id", "name": "$playlists.tracks.name",
                                     "artists": "$playlists.tracks.artists.name"}, "total": {"$sum": 1}}},
                 {"$project": {"_id": 0, "name": "$_id.name", 'artists': '$_id.artists', "total": 1}},
                 {"$sort": {"total": -1, "name": 1}}, {"$limit": 50}]
-    cursor = get_mongo_connection().aggregate(pipeline=pipeline)
+    results = collection.aggregate(pipeline=pipeline)
     logging.info("Extract done")
-    result = list(cursor)
-    return result
+    return results
 
-def transform(extracted) :
+
+def transform(docs):
     logging.info("Transform started")
-    transformed = extracted
+    transformed = [doc for doc in docs]
     logging.info("Transform done")
     return transformed
 
-def load(transformed) :
+
+def load(docs,collection_name):
+    collection = get_mongodb_connection().get_database('playrecipe').get_collection(collection_name)
     logging.info("Load started")
-    documents = transformed
-    conn = get_mongo_connection()
-    before = conn.count_documents({})
-    logging.info('before insert',before)
-    get_mongo_connection().insert_many(documents)
-    after = conn.count_documents({})
-    logging.info(after,'documents inserted')
+    before = collection.count_documents({})
+    logging.info('before insert', before)
+    collection.insert_many(docs)
+    after = collection.count_documents({})
+    logging.info(after, 'documents inserted')
     logging.info('Load stopped')
 
-def etl(**context) :
-    execution_date = context['execution_date']
+
+def etl():
+    # execution_date = context['execution_date']
     # task_instance = context['task_instance']
-    logging.info(execution_date,'ETL started')
-    extracted = extract()
+    logging.info('ETL started')
+    extracted = extract('playlist')
     transformed = transform(extracted)
-    load(transformed)
+    load(transformed,'mostchoicetracks')
     logging.info('etl done')
 
+
 mostchioce = DAG(
-    dag_id = 'most_chioce',
-    start_date = datetime(2020,9,1),
-    schedule_interval = '@daily',
-    catchup = False
+    dag_id='most_chioce',
+    start_date=datetime(2020, 9, 1),
+    schedule_interval='@daily',
+    catchup=False
 )
 
 task = PythonOperator(
-    task_id = 'most_choice_tracks',
-    python_callable = etl,
-    provide_context = True,
-    dag = mostchioce
+    task_id='most_choice_tracks',
+    python_callable=etl,
+#     provide_context=True,
+    dag=mostchioce
 )
